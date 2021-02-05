7433cd13870f5ae9f83a5a10bc458ab1552a176a
ShitBot17 <bieljapan464@gmail.com>
2021-02-04 21:43:14 -0300

    First commit

---
+++ src/darkmenu.js
@@ -0,0 +1,35 @@
+const shitmenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do SHITBOT:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+}
+
+exports.darkmenu = shitmenu
+
+
+
+
+
+
+
+
+
